7c4ea8123eb374a6cf3a29770a02f6423adf9765
bo <bojiangpro@sina.com>
2018-10-23 23:15:55 +0800

    findPeakElement

--- FindPeekElement/src/main/java/solution/Solution.java
+++ FindPeekElement/src/main/java/solution/Solution.java
@@ -2,6 +2,21 @@ package solution;
 
 class Solution {
     public int findPeakElement(int[] nums) {
-        
+        int l = 0;
+        int h = nums.length - 1;
+        int mid = h/2;
+        while(l < h)
+        {
+            if(nums[mid]<nums[mid+1])
+            {
+                l = mid+1;
+            }
+            else
+            {
+                h = mid;
+            }
+            mid = (l+h)/2;
+        }
+        return l;
     }
 }

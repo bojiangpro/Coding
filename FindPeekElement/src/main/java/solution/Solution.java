423def5ea455ba83e958c9f30fee6d840a61d207
bo <bojiangpro@sina.com>
2018-11-03 19:35:13 +0800

    find peak element 2d

--- FindPeekElement/src/main/java/solution/Solution.java
+++ FindPeekElement/src/main/java/solution/Solution.java
@@ -19,4 +19,36 @@ class Solution {
         }
         return l;
     }
+
+    public int[] find2DPeakElement(int[][] nums) {
+        int l = 0;
+        int h = nums.length - 1;
+        int mid = h/2;
+        while(l < h)
+        {
+            int m = findMaxElement(nums[mid]);
+            if(nums[mid][m]<nums[mid+1][m])
+            {
+                l = mid+1;
+            }
+            else
+            {
+                h = mid;
+            }
+            mid = (l+h)/2;
+        }
+
+        return new int[] {l, findMaxElement(nums[l])};
+    }
+
+    private static int findMaxElement(int[] nums){
+        int max = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i] > nums[max])
+            {
+                max = i;
+            }
+        }
+        return max;
+    }
 }
--- FindPeekElement/src/test/java/solution/SolutionTest.java
+++ FindPeekElement/src/test/java/solution/SolutionTest.java
@@ -1,5 +1,6 @@
 package solution;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
@@ -8,45 +9,30 @@ import org.junit.Test;
 public class SolutionTest{
 
     private Solution solution;
-    private ListNode[] nodes;
-
     @Before
     public void setUp()
     {
         this.solution = new Solution();
-        this.nodes = new ListNode[100];
-        for (int i = 0; i < 100; i++) {
-            this.nodes[i] = new ListNode(i);
-        }
     }
 
     @Test
     public void basicTest()
     {
-        test(new int[]{1,3,5,7,9,11,13,15,17,19,21}, new int[]{2}, null);
-        setUp();
-        test(new int[]{2, 4, 5,1}, new int[]{5,1}, 5);
+        test(new int[]{1,3,5,7,9,11,16,15,14,13,12}, 6);
+        test(new int[][]{{2, 4, 5, 1}, 
+                         {1, 7, 6, 2},
+                         {3, 4, 5, 1} }, new int []{1, 1});
     }
 
-    private void test(int[] first, int[] second, Integer expected)
+    private void test(int[] nums, int expected)
     {
-
-        ListNode l1 = createList(first);
-        ListNode l2 = createList(second);
-        ListNode out = this.solution.getIntersectionNode(l1, l2);
-        ListNode node = expected == null ? null : this.nodes[expected];
-        assertEquals(node, out);
+        int max = this.solution.findPeakElement(nums);
+        assertEquals(max, expected);
     }
 
-    private ListNode createList(int[] values)
+    private void test(int[][] nums, int[] expected)
     {
-        ListNode h = new ListNode(0);
-        ListNode f = h;
-        for (int v : values) 
-        {
-            h.next = this.nodes[v];
-            h = h.next;
-        }
-        return f.next;
+        int[] max = this.solution.find2DPeakElement(nums);
+        assertArrayEquals(max, expected);
     }
 }
